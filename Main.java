654617fc55ea802b4c81f6bb3db87dbe0f8ca920
jacoe0850 <jacoe0850@wrdsb.ca>
2020-10-02 22:31:06 +0000

    finished max integer

--- Main.java
+++ Main.java
@@ -1,15 +1,26 @@
+import java.util.Scanner;
 /**
  *
- * @author 
+ * @jacoe0850 
  */
 public class Main {
 
-  /**
-   * The method that is executed when you hit the run button.
-   * @param args the command line arguments
-   */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+   // make scanner for input
+    Scanner input = new Scanner(System.in);
+    // ask for intergers
+    System.out.println("Please enter three integers on separate lines");
+    int num = input.nextInt();
+    int num1 = input.nextInt();
+    int num2 = input.nextInt();
+    // decides maximum number
+    if(num >= num && num >= num2){
+      System.out.println("Maximum:" + " " + num);
+    } else if(num1 >= num && num1 >= num2){
+    System.out.println("Maximum: " + " " + num1);
+  } else if(num2 >= num && num2 >= num1){
+  System.out.println("Maximum" + " " + num2);
+}
+
   }
 }
